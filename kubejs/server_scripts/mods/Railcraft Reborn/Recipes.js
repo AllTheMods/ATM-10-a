1b3a8a7bcbf0ad0198ab91119f4dc5142b2ab3f2
oly2o6 <calumroberts02@hotmail.com>
2026-08-20 00:19:44 +0100

    Simplified steam turbine recipe

---
+++ kubejs/server_scripts/mods/Railcraft Reborn/Recipes.js
@@ -0,0 +1,27 @@
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+
+ServerEvents.recipes(allthemods => {
+    allthemods.remove({ id: `railcraft:steam_turbine` })
+    allthemods.shaped('railcraft:steam_turbine', [
+        'AAA',
+        'BCB',
+        'AAA'
+    ], {
+        A: 'alltheores:steel_plate',
+        B: 'railcraft:high_pressure_steam_boiler_tank',
+        C: 'alltheores:steel_block'
+    })
+
+    allthemods.remove({ id: `railcraft:rolling/steel_turbine_blade` })
+    allthemods.shaped('2x railcraft:turbine_blade', [
+        '  A',
+        ' A ',
+        'A  '
+    ], {
+        A: 'alltheores:steel_plate'
+    })
+})
+
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
