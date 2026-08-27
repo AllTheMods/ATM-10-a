8d8ca7c5d14da825e72e8b1dfac430c817764cbe
oly2o6 <calumroberts02@hotmail.com>
2026-08-27 23:28:26 +0100

    dissolution temp fix

--- kubejs/server_scripts/mods/Indusrial Foregoing/Dissolution Chamber.js
+++ kubejs/server_scripts/mods/Indusrial Foregoing/Dissolution Chamber.js
@@ -2,24 +2,50 @@
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
 
 ServerEvents.recipes(allthemods => {
-    // input[], input_fluid, processing_time, output, outputfluid
-    let dissolution_chamber = allthemods.recipes.industrialforegoing.dissolution_chamber
+    function dissolution_chamber(output, inputs, fluid, amount, time, id) {
+        let recipe = {
+            type: 'industrialforegoing:dissolution_chamber',
+            input: [],
+            inputFluid: fluid.startsWith('#')
+                ? { tag: fluid.substring(1), amount: amount }
+                : { fluid: fluid, amount: amount },
+            processingTime: time,
+            output: {
+                count: output.count || 1,
+                id: output.item
+            }
+        }
 
-    dissolution_chamber([
-        '#c:glass_blocks/colorless'
-    ],
-        "2700x industrialforegoing:pink_slime", 
+        inputs.forEach(input => {
+            let ingredient = {}
+            if (input.tag) {
+                ingredient.tag = input.tag
+            } else {
+                ingredient.item = input.item
+            }
+            recipe.input.push(ingredient)
+        })
+
+        allthemods.custom(recipe).id(id)
+    }
+
+    dissolution_chamber(
+        { item: 'industrialforegoing:pink_slime_block' },
+        [{ tag: 'c:glass_blocks/colorless' }],
+        'industrialforegoing:pink_slime',
+        2700,
         200,
-        "industrialforegoing:pink_slime_block"
+        'kubejs:dissolution_chamber/pink_slime_block'
     )
 
-    dissolution_chamber([
-        'minecraft:glass_bottle'
-    ],
-        "250x #c:experience",
+    dissolution_chamber(
+        { item: 'minecraft:experience_bottle' },
+        [{ item: 'minecraft:glass_bottle' }],
+        '#c:experience',
+        250,
         5,
-        "minecraft:experience_bottle"
-    ).id("industrialforegoing:dissolution_chamber/xp_bottles")
+        'industrialforegoing:dissolution_chamber/xp_bottles'
+    )
 })
 
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
