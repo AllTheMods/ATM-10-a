67497940dc46df355651931a9ca6b067fd66e3cb
Kim Jinsu <item4hun@gmail.com>
2026-09-16 16:10:47 +0900

    [Cross-port] Blacklist Neo Vitae Imprisonment Array from certain mobs
    https://github.com/AllTheMods/ATM-10/pull/4382
    
    Co-authored-by: DivineFinal <vidabence6@gmail.com>

--- kubejs/server_scripts/Tweaks/tags.js
+++ kubejs/server_scripts/Tweaks/tags.js
@@ -190,7 +190,10 @@ ServerEvents.tags('entity_type', allthemods => {
   allthemods.add('ars_additions:source_spawner_denylist', '#allthemods:jank_blacklist')
   allthemods.add('oritech:spawner_blacklist', '#allthemods:jank_blacklist')
   allthemods.add('occultism:soul_shattered_deny_list', '#allthemods:jank_blacklist')  
-
+  allthemods.add('neovitae:deny_imprisonment', [
+	  "#c:capturing_not_supported",
+	  "#apothic_spawners:blacklisted_from_spawners"
+  ])
 })
 
 ServerEvents.tags('worldgen/structure', allthemods => {
